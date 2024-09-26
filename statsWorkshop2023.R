a8bfe26c98254a6a281ce0edf63bbfa1beefd19a
Nikolas Giannakis <ngiannak@exseed.ed.ac.uk>
2024-09-26 10:59:13 +0100

    code update

---
+++ statsWorkshop2023.R
@@ -0,0 +1,11 @@
+---
+  title: "Research Skills Course - Statistics Workshop"
+author: "Nikolas Giannakis & Florent Petitprez"
+date: "January / February 2023"
+output:
+  html_document:
+  toc: true
+toc_float: true
+code_folding: show
+lang: "en"
+---
